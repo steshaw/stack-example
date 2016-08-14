1efc1732343a778ee7dd79a1b9fd4dc211baff82
Steven Shaw <steven@steshaw.org>
2016-08-14 23:33:09 +1000

    Little nixos.org example program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Lib
+import Network.HTTP
+import Text.HTML.TagSoup
 
 main :: IO ()
-main = someFunc
+main = do
+  resp <- Network.HTTP.simpleHTTP (getRequest "http://nixos.org")
+  body <- getResponseBody resp
+  let tags = filter (isTagOpenName "a") $ parseTags body
+  let tags' = map (fromAttrib "href") tags
+  mapM_ putStrLn $ filter (/= "") tags'
